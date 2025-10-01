b7e376bd4d2de2cbb0761bba3357c4026bd95af5
rohan <rohan14012004@gmail.com>
2025-10-01 14:24:45 +0530

    Still in testing

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,5 @@
 from website import create_app
 from waitress import serve
-
-
-app = create_app()
-
-if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=5000)
-from website import create_app
-from waitress import serve
 import os
 
 app = create_app()
