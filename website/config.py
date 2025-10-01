cfdd2c2adb12150972bb72e6eec67729e5ef702f
rohan <rohan14012004@gmail.com>
2025-10-01 14:23:06 +0530

    Ready for production deployment

--- website/config.py
+++ website/config.py
@@ -2,5 +2,11 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev_key")
+
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+    if not DATABASE_URL:
+
+        DATABASE_URL = os.path.join(os.getcwd(), "website.db")
+
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{DATABASE_URL}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///website.db")
--- wsgi.py
+++ wsgi.py
@@ -6,3 +6,12 @@ app = create_app()
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=5000)
+from website import create_app
+from waitress import serve
+import os
+
+app = create_app()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    serve(app, host="0.0.0.0", port=port)
